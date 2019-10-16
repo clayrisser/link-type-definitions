94cc07e7095be2816296904309b3d7943f5bf9b1
Jam Risser <jam@codejam.ninja>
2019-10-16 03:01:35 -0500

    Fixed unit tests

--- tests/util.ts
+++ tests/util.ts
@@ -1,9 +1,10 @@
-import { getPackageUrl } from '../src/util';
+import { getPackageInfo } from '../src/util';
 
 describe('async getPackageUrl(key, value)', () => {
   it('it should get npm module', async () => {
-    expect(await getPackageUrl('@types/node', 'latest')).toBe(
-      'https://npmjs.org/package/@types/node'
-    );
+    expect(await getPackageInfo('@types/node', '12.11.1')).toEqual({
+      name: '@types/node',
+      url: 'https://registry.npmjs.org/@types/node/-/node-12.11.1.tgz'
+    });
   });
 });
