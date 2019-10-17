233a5fb24426448e08154463f2a666e716449de0
Jam Risser <jam@codejam.ninja>
2019-10-17 14:51:36 -0500

    v0.1.8

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -6,18 +6,8 @@ import { Command, flags } from '@oclif/command';
 import { oc } from 'ts-optchain.macro';
 import { install } from '../util';
 
-export function getDefinitionsPath(cwd: string): string {
-  const nodeModulesPath = path.resolve(cwd, '../../node_modules');
-  if (
-    fs.existsSync(nodeModulesPath) &&
-    fs.lstatSync(nodeModulesPath).isDirectory()
-  ) {
-    return getDefinitionsPath(path.resolve(cwd, '../../..'));
-  }
-  return pkgDir.sync(cwd) || cwd;
-}
-
-const rootPath = getDefinitionsPath(process.cwd());
+const modulePath = pkgDir.sync(process.cwd()) || process.cwd();
+const rootPath = modulePath.replace(/\/node_modules\/.+$/, '');
 const defintionsPath = path.resolve(rootPath, 'node_modules/@types/_');
 const tmpPath = path.resolve(rootPath, '.tmp/tspm');
 
@@ -34,7 +24,7 @@ export default class Install extends Command {
 
   async run() {
     const spinner = ora();
-    const pkg = await import(path.resolve(rootPath, 'package.json'));
+    const pkg = await import(path.resolve(modulePath, 'package.json'));
     spinner.start('installing type definitions');
     await fs.remove(tmpPath);
     await fs.remove(defintionsPath);
